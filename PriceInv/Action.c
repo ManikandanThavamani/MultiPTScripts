b7846e7b56106a5d167e85af5088e753e1519b90
ManikandanThavamani <tmanikandan55@gmail.com>
2026-05-19 13:54:49 +0530

    Added transactions

--- PriceInv/Action.c
+++ PriceInv/Action.c
@@ -10,7 +10,7 @@ Action()
 		SEARCH_FILTERS,
 		LAST);
 
-	
+	lr_start_transaction("S1_TR01_Auth");
 	web_submit_data("Auth",
 	     "Action=https://fedloginqa.cat.com/as/token.oauth2",
 	     "Method=POST",
@@ -19,14 +19,17 @@ Action()
 	     "Name=client_id","Value=CAT-RDE_cc_client",ENDITEM,
 	     "Name=client_secret","Value=ZgSi2Zj8DjjVDveA3PIKlh8axCUkw6NA3BtYov0NHnu4gm78zVrUhyVvwtiPWCSC",ENDITEM,
 	     LAST);
+	lr_end_transaction("S1_TR01_Auth",LR_AUTO);
 	
 	web_add_header("Authorization","Bearer {c_AuthToken}");
 	web_add_header("Content-Type","application/json");
 	
+	lr_start_transaction("S1_TR02_ActualAPI");
 	web_custom_request("ActualPriceAPI",
 	                   "URL=https://api-performance.cat.com/rde/dealerSalesModels/v1/dealer/priceAndInventories/search",
 	                   "Method=POST",
 	                   "Body={\"dealerCode\": \"H160\",\"rdeSalesModels\": [\"255\"],\"region\": \"ADSD-N\",\"locale\": \"en_US\"}",
 	                   LAST);
+	lr_end_transaction("S1_TR02_ActualAPI",LR_AUTO);
 	return 0;
 }
